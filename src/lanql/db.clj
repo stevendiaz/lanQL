db8e5155466370ca6fadf05094735c5da22c538a
Steven Diaz <stevendiaz@utexas.edu>
2018-08-18 18:56:51 -0700

    delete extraneous code from example

--- src/lanql/db.clj
+++ src/lanql/db.clj
@@ -25,15 +25,6 @@
   []
   {:db (map->LanDb {})})
 
-(defn find-game-by-id
-  [db game-id]
-  (->> db
-       :data
-       deref
-       :games
-       (filter #(= (str game-id) (:id %)))
-       first))
-
 (defn find-user-by-id
   [component user-id]
   (-> (jdbc/query
@@ -42,62 +33,3 @@
                      :from [:users_user]
                      :where [:= :id user-id]}))
       first))
-
-
-(defn find-member-by-id
-  [db member-id]
-  (->> db
-       :data
-       deref
-       :members
-       (filter #(= (str member-id) (:id %)))
-       first))
-
-(defn list-designers-for-game
-  [db game-id]
-  (let [designers (:designers (find-game-by-id db game-id))]
-    (->> db
-         :data
-         deref
-         :designers
-         (filter #(contains? designers (:id %))))))
-
-(defn list-games-for-designer
-  [db designer-id]
-  (->> db
-       :data
-       deref
-       :games
-       (filter #(-> % :designers (contains? designer-id)))))
-
-(defn list-ratings-for-game
-  [db game-id]
-  (->> db
-       :data
-       deref
-       :ratings
-       (filter #(= (str game-id) (:game_id %)))))
-
-(defn list-ratings-for-member
-  [db member-id]
-  (->> db
-       :data
-       deref
-       :ratings
-       (filter #(= (str member-id) (:member_id %)))))
-
-(defn ^:private apply-game-rating
-  [game-ratings game-id member-id rating]
-  (->> game-ratings
-       (remove #(and (= game-id (:game_id %))
-                     (= member-id (:member_id %))))
-       (cons {:game_id game-id
-              :member_id member-id
-              :rating rating})))
-
-(defn upsert-game-rating
-  "Adds a new game rating, or changes the value of an existing game rating."
-  [db game-id member-id rating]
-  (-> db
-      :data
-      (swap! update :ratings apply-game-rating game-id member-id rating)))
